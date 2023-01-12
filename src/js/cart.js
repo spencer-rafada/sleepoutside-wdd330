b73264265fd50ab45d839bb5afc0051944ab30da
Vladlen <suprime34515@icloud.com>
2023-01-11 23:31:30 -0700

    Add to Cart button fixed!

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,20 @@
-import { getLocalStorage } from "./utils.mjs";
 
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+// get info from local storage
+function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+// function that has an arry with the info from local storage
+function getCartContents() {
+  let markup = '';
+  const cartItems = getLocalStorage('so-cart');
+  const cart =  [];
+  cart.push(cartItems)
+  const htmlItems = cart.map((item) => renderCartItem(item));
+  document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
-function cartItemTemplate(item) {
+function renderCartItem(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
@@ -21,8 +29,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-
   return newItem;
 }
 
-renderCartContents();
+getCartContents();
