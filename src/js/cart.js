0490528abfcd022d759cfbcbbca9304dfdfbf22f
spencer-rafada <spencer.byui@gmail.com>
2023-01-18 11:12:29 -0700

    added remove to cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,27 @@
 function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
+// set info to local storage
+function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+// Render Items for the first load
+window.addEventListener(`load`, () => {
+  getCartContents();
+});
+
+// Remove items from the cart event listener
+window.addEventListener(`click`, () => {
+  document.querySelectorAll(`[data-id]`).forEach((item) => {
+    item.addEventListener(`click`, removeClickedHandler);
+  });
+});
+
 // function that has an arry with the info from local storage
 function getCartContents() {
+  document.querySelector(".product-list").innerHTML = "";
   let markup = "";
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => renderCartItem(item));
@@ -11,7 +30,7 @@ function getCartContents() {
   // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
-//renders the items
+// renders the items
 function renderCartItem(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
@@ -26,8 +45,17 @@ function renderCartItem(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="cart-card__remove" data-id=${item.Id}>X</span>
 </li>`;
   return newItem;
 }
 
-getCartContents();
+// Event Handler for clicking remove from cart
+const removeClickedHandler = (event) => {
+  const selectId = event.target;
+  var cartItems = getLocalStorage(`so-cart`).filter(
+    (item) => item.Id !== selectId.dataset.id
+  );
+  setLocalStorage(`so-cart`, cartItems);
+  getCartContents();
+};
