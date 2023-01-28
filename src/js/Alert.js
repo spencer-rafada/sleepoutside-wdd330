8fd1b0706c5b9b8a1dd3008613a6d1481eae2e78
spencer-rafada <spencer.byui@gmail.com>
2023-01-28 10:14:26 -0700

    added alert feature

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,1 +1,50 @@
-export default class Alert {}
+import convertToJson from "./utils.mjs";
+export default class Alert {
+  constructor() {
+    this.path = `../json/alerts.json`;
+    this.mainElement = document.querySelector("main");
+    this.init();
+  }
+
+  async init() {
+    this.alerts = await this.getData();
+    this.mainElement.insertAdjacentHTML(`beforebegin`, this.alertTemplate());
+    this.renderAlertList();
+  }
+
+  // get alerts.json data
+  getData() {
+    return fetch(this.path)
+      .then(convertToJson)
+      .then((data) => data);
+  }
+
+  // alert template
+  alertTemplate() {
+    return `<section class="alert-list">
+    </section>`;
+  }
+
+  renderAlertList() {
+    const alertList = this.alerts.map(
+      (alert) =>
+        `<p class="alert" style="background-color:${alert.background};color:${alert.color};">${alert.message}</p>`
+    );
+    const element = document.querySelector(`.alert-list`);
+    // send alerts after 2 seconds
+    setTimeout(() => {
+      element.innerHTML = alertList.join(``);
+      this.removeAlertList();
+    }, 2000);
+  }
+
+  removeAlertList() {
+    const parent = document.querySelector(`.alert-list`);
+    const elements = document.querySelectorAll(`.alert`);
+    elements.forEach((item) => {
+      setTimeout(() => {
+        parent.removeChild(item);
+      }, 10000);
+    });
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
-const source = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const list = new ProductListing("tents", source, element);
+import Alert from "./Alert.js";
 
-list.init();
+const alerts = new Alert();
 
+const dataSource = new ProductData("tents");
+const productListElement = document.querySelector(".product-list");
+const productList = new ProductListing("tents", dataSource, productListElement);
+
+productList.init();
