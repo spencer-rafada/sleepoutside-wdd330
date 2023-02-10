53e318feca9313123c13ed2565eef43df8841b14
spencer-rafada <spencer.byui@gmail.com>
2023-02-10 12:02:51 -0700

    netlify test

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     outDir: "dist/",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "/index.html"),
-        cart: resolve(__dirname, "/cart/index.html"),
-        product: resolve(__dirname, "/product_pages/index.html"),
-        "product-listing": resolve(__dirname, "/product-listing/index.html"),
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        "product-listing": resolve(__dirname, "src/product-listing/index.html"),
       },
     },
   },
